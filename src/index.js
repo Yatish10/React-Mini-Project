73530fc7f31ad1c5a1dbc700870f03c4758ac8c1
Yatish10 <yatish.chaudhari108@gmail.com>
2022-12-20 15:10:40 +0530

    Simple Greeting Project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Show Heading that says Hello Sir, Good Morning, If Time is Between 1 to 11am
+// Good Afternoon, if 12pm to 7pm
+// Good Night, If 7pm to midnight
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+
+let curDate = new Date(2022,12,20,15);
+curDate = curDate.getHours();
+let greeting = '';
+
+const cssStyle = { };
+
+if(curDate >= 1 && curDate < 12){
+  greeting = 'Good Morning !';
+  cssStyle.color = 'blue';
+
+} else if(curDate >= 12 && curDate < 20){
+  greeting = 'Good Afternoon !';
+  cssStyle.color = 'orange';
+
+}else{
+  greeting = 'Good Night !';
+  cssStyle.color = 'green';
+}
+
+ReactDOM.render(
+  <>
+    <div>
+      <h1> Hello Sir, <span style= {cssStyle}> { greeting } </span></h1>
+    </div>
+  </>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
