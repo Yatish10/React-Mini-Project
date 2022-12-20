8531839003b80f7f52f356e9282c743517c4c3f6
Yatish10 <yatish.chaudhari108@gmail.com>
2022-12-20 19:02:37 +0530

    Netflix Project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Card from "./Cards";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( 
+<>
+  <Card 
+    imgsrc = "dark.jpg"
+    title = " A Netflix Orignal Series"
+    sname = "DARK"
+    link = "https://www.netflix.com/in/title/80990668?source=35"
+  /> 
+  <Card 
+    imgsrc = "extracurricular.jpg"
+    title = " A Netflix Orignal Series"
+    sname = "Extra Curricular"
+    link = "https://www.netflix.com/in/title/80990668"
+  />
+  <Card 
+    imgsrc = "strangerthings.jpg"
+    title = " A Netflix Orignal Series"
+    sname = "Stranger Things"
+    link = "https://www.netflix.com/in/title/80057281"
+
+  />
+</>, 
+document.getElementById("root"));
 
