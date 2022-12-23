dbd24e1d41e6d6ea3d05c20b064137356e9e181a
Yatish10 <yatish.chaudhari108@gmail.com>
2022-12-23 13:22:42 +0530

    Slot Machine Project Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SlotM from "./SlotMach";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading_style" > 
+        🎰 Welcome to {" "} 
+        <span style= {{ fontWeight: "bold" }}> Slot Machine Game</span> 🎰 {" "}
+      </h1>
+      <div className="slotmachine">
+      <SlotM x= '😄' y= '😄' z= '😄' />
+      <SlotM x= '🍌' y= '🤟' z= '😃' />
+      <SlotM x= '🍎' y= '👍' z= '💯' />
+      <SlotM x= '💑' y= '💑' z= '💑' />
+      </div>
+      
+    </>
   );
 }
+
 
 export default App;
